55a5bc67267d9e7b2fa17d3724e3f4bfe52db3b7
Maxim Timokhin <mtimokhin@fun.co>
2021-03-06 18:15:39 +0300

    app state

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ import time
 from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 from random import randrange
 from enum import Enum
+from picamera import PiCamera
+from playground.barrier.find_way import find_distances
+from playground.grid.count_grid import calculate_grid, calculate_intersect_grid
+import numpy as np
 
 try:
     import controls.servo as servo
@@ -23,15 +27,20 @@ class State:
     green_angle: int
     grid_result: [int]
     distance: float
+    current_image = None
 
     def asString(self):
         return f"green = {self.green_angle}, grid = {self.grid_result}, dist = {self.distance}"
+
+
+grid, result_grid = calculate_grid(True)
 
 
 class App:
     mode: Mode
     state: State
     handlers: {}
+    executor = PoolExecutor(max_workers=4)
 
     def __init__(self):
         self.state = State()
@@ -40,26 +49,22 @@ class App:
         self.handlers = {}
 
     def run(self):
+        self.start_input_measure()
         try:
             while True:
-                self.state = self.get_next_state()
                 self.mode = self.get_next_mode()
                 print(f"{self.mode} {self.state.asString()}")
+                time.sleep(1)
         except KeyboardInterrupt:
             print("main.Closed")
         finally:
             distance.cleanup()
 
-    def get_next_state(self):
-        state = State()
-        with PoolExecutor(max_workers=3) as executor:
-            distance = executor.submit(self.measure_distance)
-            green_angle = executor.submit(self.follow_green)
-            grid_result = executor.submit(self.grid_calculate)
-            state.distance = distance.result()
-            state.green_angle = green_angle.result()
-            state.grid_result = grid_result.result()
-        return state
+    def start_input_measure(self):
+        self.executor.submit(self.measure_distance)
+        self.executor.submit(self.follow_green)
+        self.executor.submit(self.grid_calculate)
+        self.executor.submit(self.capture_camera)
 
     def get_next_mode(self):
         handlers = {
@@ -82,13 +87,36 @@ class App:
         return Mode.PARKING
 
     def measure_distance(self):
-        return distance.getDistance()
+        try:
+            while True:
+                try:
+                    self.state.distance = distance.getDistance()
+                except:
+                    continue
+        finally:
+            distance.cleanup()
 
     def follow_green(self):
-        # do green point angle calculation here
-        return randrange(-100, 100)
+        while True:
+            self.state.green_angle = randrange(-100, 100)
 
     def grid_calculate(self):
-        # do grid result calculation here
-        time.sleep(1)
-        return [randrange(-100, 100), randrange(-100, 100)]
+        while True:
+            if self.state.current_image is not None:
+                calculate_intersect_grid(self.state.current_image, grid, result_grid)
+                self.state.grid_result = find_distances(result_grid)
+
+    def capture_camera(self):
+        try:
+            camera
+        except NameError:
+            camera = PiCamera()
+            camera.resolution = (320, 240)
+            camera.framerate = 60
+            camera.start_preview()
+            time.sleep(2)
+        image = np.empty((240 * 320 * 3,), dtype=np.uint8)
+        image = image.reshape((240, 320, 3))
+        index = 1
+        for _ in camera.capture_continuous(image, format='bgr', use_video_port=True):
+            self.state.current_image = image
